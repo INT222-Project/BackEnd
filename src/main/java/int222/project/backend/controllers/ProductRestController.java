d01d8e0d3b76a79ba8bfb902a9a2bbbed6cf94a1
62130500093 <sahachai.kmutt@mail.kmutt.ac.th>
2021-10-02 15:57:24 +0700

    [add] cross origin

--- src/main/java/int222/project/backend/controllers/ProductRestController.java
+++ src/main/java/int222/project/backend/controllers/ProductRestController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = {""},allowedHeaders = "*")
+@CrossOrigin(origins = {"http://localhost:8081"},allowedHeaders = "*")
 @RestController
 public class ProductRestController {
     @Autowired
