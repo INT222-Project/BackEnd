246cd5ed4f0bda31eb48bd099fb583c756b896ef
62130500093 <sahachai.kmutt@mail.kmutt.ac.th>
2021-10-16 17:20:15 +0700

    [add] add method in reservation repo

--- src/main/java/int222/project/backend/repositories/ReservationRepository.java
+++ src/main/java/int222/project/backend/repositories/ReservationRepository.java
@@ -2,5 +2,11 @@ package int222.project.backend.repositories;
 
 import int222.project.backend.models.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ReservationRepository extends JpaRepository<Reservation, String> { }
+import java.util.List;
+
+public interface ReservationRepository extends JpaRepository<Reservation, String> {
+    @Query("select rs from Reservation rs where rs.repId is null")
+    List<Reservation> getUnsuccessReservation();
+}
