0ef57d1ff50354912dac473c0e52b26cf37cec44
62130500093 <sahachai.kmutt@mail.kmutt.ac.th>
2021-11-18 23:47:01 +0700

    [updat]

--- src/main/java/int222/project/backend/config/WebSecurityConfig.java
+++ src/main/java/int222/project/backend/config/WebSecurityConfig.java
@@ -42,7 +42,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         // We don't need CSRF for this example
-        httpSecurity.csrf().disable()
+        httpSecurity.cors().and().csrf().disable()
                 // dont authenticate this particular request
                 .authorizeRequests().antMatchers("/api/auth/authenticate").permitAll().
                 // all other requests need to be authenticated
