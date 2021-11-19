ae68aa703d201eda744e3a16d997f4c13335aee5
62130500093 <sahachai.kmutt@mail.kmutt.ac.th>
2021-11-19 14:36:33 +0700

    [add] configuration webSecurity config request mapping

--- src/main/java/int222/project/backend/config/WebSecurityConfig.java
+++ src/main/java/int222/project/backend/config/WebSecurityConfig.java
@@ -42,11 +42,18 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         // We don't need CSRF for this example
-        httpSecurity.cors().and().csrf().disable()
+        httpSecurity.cors().and().csrf().disable();
                 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/api/auth/authenticate").permitAll().
-                // all other requests need to be authenticated
-                        anyRequest().authenticated().and().
+        httpSecurity.authorizeRequests().antMatchers("/api/auth/authenticate").permitAll();
+        httpSecurity.authorizeRequests().antMatchers("/api/roomTypes").permitAll();
+        httpSecurity.authorizeRequests().antMatchers("/api/packages").permitAll();
+        httpSecurity.authorizeRequests().antMatchers("/api/rooms/showImage/{roomid}").permitAll();
+        httpSecurity.authorizeRequests().antMatchers("/api/roomTypes/{roomtypeid}").permitAll();
+        httpSecurity.authorizeRequests().antMatchers("/api/rooms/roomRequirement/**").permitAll();
+//        httpSecurity.authorizeRequests().antMatchers("/api/rooms/getRemainingRoom/{roomtypeid}").permitAll();
+        httpSecurity.authorizeRequests().antMatchers("/api/rooms/getRemainingRoom/**").permitAll();
+        // all other requests need to be authenticated
+        httpSecurity.authorizeRequests().anyRequest().authenticated().and().
                 // make sure we use stateless session; session won't be used to
                 // store user's state.
                         exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
